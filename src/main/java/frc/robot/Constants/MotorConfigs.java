718eb4c127c6ef944f02ba4c21e4574bf53bfc9c
LtBrain <brian.j.guan@outlook.com>
2026-03-10 02:59:55 -0400

    basic shooter pid and some subsystem stuff

--- src/main/java/frc/robot/Constants/MotorConfigs.java
+++ src/main/java/frc/robot/Constants/MotorConfigs.java
@@ -15,6 +15,8 @@ import com.thethriftybot.devices.ThriftyNova.ThriftyNovaConfig;
 /** Motor Configurations */
 public interface MotorConfigs {
 
+    
+
     public interface Climber {
         SparkBaseConfig MOTOR_CONFIG = new SparkMaxConfig()
             .smartCurrentLimit(40)
@@ -43,19 +45,47 @@ public interface MotorConfigs {
     }
 
     public interface Shooter {
-        SparkBaseConfig RIGHT_FLYWHEEL_MOTOR_CONFIG = new SparkMaxConfig()
-            .smartCurrentLimit(60)
-            .idleMode(IdleMode.kCoast);
 
-        SparkBaseConfig LEFT_FLYWHEEL_MOTOR_CONFIG = new SparkMaxConfig()
-            .smartCurrentLimit(60)
-            .idleMode(IdleMode.kCoast)
-            .follow(25, true);
+        SparkBaseConfig RIGHT_FLYWHEEL_MOTOR_CONFIG = createRightFlywheelConfig();
+
+        private static SparkBaseConfig createRightFlywheelConfig() {
+            SparkBaseConfig config = new SparkMaxConfig()
+                .smartCurrentLimit(60)
+                .idleMode(IdleMode.kCoast)
+                .voltageCompensation(12);
+
+            // configure PID here
+            config.closedLoop
+                .feedbackSensor(FeedbackSensor.kAlternateOrExternalEncoder)
+                .pid(0.00025, 0.0, 0.0)
+                .velocityFF(0.00018)
+                .outputRange(-1, 1);
+
+            return config;
+        }
+
+        SparkBaseConfig LEFT_FLYWHEEL_MOTOR_CONFIG = createLeftFlywheelConfig();
+
+        private static SparkBaseConfig createLeftFlywheelConfig() {
+            SparkBaseConfig config = new SparkMaxConfig()
+                .smartCurrentLimit(60)
+                .idleMode(IdleMode.kCoast)
+                .voltageCompensation(12)
+                .follow(25, true);
+
+            config.closedLoop
+                .feedbackSensor(FeedbackSensor.kAlternateOrExternalEncoder)
+                .pid(0.00025, 0.0, 0.0)
+                .velocityFF(0.00018)
+                .outputRange(-1, 1);
+
+            return config;
+        }
 
         SparkBaseConfig HOOD_MOTOR_CONFIG = new SparkMaxConfig()
             .smartCurrentLimit(20)
             .idleMode(IdleMode.kBrake);
-        
+
         EncoderConfig FLYWHEEL_ENCODER_CONFIG = new EncoderConfig()
             .countsPerRevolution(8192);
     }
--- src/main/java/frc/robot/subsystems/Mechanisms/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/Mechanisms/ShooterSubsystem.java
@@ -15,9 +15,13 @@ import frc.robot.Constants.MotorConfigs;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkMaxAlternateEncoder;
 import com.revrobotics.spark.SparkRelativeEncoder;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
+
+import java.io.ObjectInputFilter.Config;
 
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.PersistMode;
@@ -32,7 +36,7 @@ public class ShooterSubsystem extends SubsystemBase {
 
   private RelativeEncoder FlywheelEncoder;
 
-  private final SparkPIDController FlywheelPID = RightFlywheelMotor.getPIDController();
+  private final SparkClosedLoopController FlywheelPID = RightFlywheelMotor.getClosedLoopController();
 
   public ShooterSubsystem() {
 
@@ -53,11 +57,33 @@ public class ShooterSubsystem extends SubsystemBase {
     //Apply configs to devices
     LeftFlywheelMotor.configure(Shooter.LEFT_FLYWHEEL_MOTOR_CONFIG, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     RightFlywheelMotor.configure(Shooter.RIGHT_FLYWHEEL_MOTOR_CONFIG, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    HoodMotor.configure(Shooter.HOOD_MOTOR_CONFIG, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    HoodMotor.configure(Shooter.HOOD_MOTOR_CONFIG, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);    
   }
 
-  public void setShooterSpeed(double velocity) {
-    RightFlywheelMotor.set(velocity);
+  public void setShooterSpeed(double shooterRPM) {
+    FlywheelPID.setSetpoint(shooterRPM, ControlType.kVelocity);
+  }
+
+  public void setShooterIdle() {
+    FlywheelPID.setSetpoint(750, ControlType.kVelocity);
+  }
+
+  public void setShooterStop() {
+    FlywheelPID.setSetpoint(0, ControlType.kVelocity);
+    RightFlywheelMotor.stopMotor();
+  }
+
+  public boolean shooterAtSpeed() {
+      // Check if the flywheel is within the acceptable error range of the target RPM
+      double currentRPM = FlywheelEncoder.getVelocity();
+      double targetRPM = FlywheelPID.getSetpoint();
+      double error = Math.abs(targetRPM - currentRPM);
+
+      if (error <= 75) {
+        return true;
+      } else {
+        return false;
+      }
   }
 
   @Override
