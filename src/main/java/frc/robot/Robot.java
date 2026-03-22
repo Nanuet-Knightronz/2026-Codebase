2c91aee35c8b7e4fcb135ad21f6a5b7a412ccdda
LtBrain <brian.j.guan@outlook.com>
2026-03-22 19:46:02 -0400

    add alliance flipping for yagsl

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Constants.Constants;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as
@@ -136,6 +137,7 @@ public class Robot extends TimedRobot
     {
       CommandScheduler.getInstance().cancelAll();
     }
+    m_robotContainer.zeroGyroWithAlliance();
   }
 
   /**
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -253,4 +253,8 @@ public class RobotContainer
   {
     drivebase.setMotorBrake(brake);
   }
+
+  public void zeroGyroWithAlliance() {
+    drivebase.zeroGyroWithAlliance();
+  }
 }
